ae0488d4589c82d4424c7b91937094dab3bd7de4
Caglar <asdwas94@hotmail.com>
2021-08-22 19:35:40 +0300

    Updated Sudoku cell to playable state

--- Sudoku/Assets/Scripts/SudokuCell.cs
+++ Sudoku/Assets/Scripts/SudokuCell.cs
@@ -7,8 +7,14 @@ public class SudokuCell : Selectable
     /// The cell's location
     public Vector2 ID { get; private set; }
 
-    /// The cell's number
-    public int Number { get; private set; }
+    /// The cell's actual number
+    public int Answer { get; private set; }
+
+    /// The cell's current number according to the player's input
+    public int Value { get; private set; }
+
+    /// Is this cell a clue at the start of the game 
+    public bool IsClue { get; private set; }
 
     private TextMeshProUGUI _numberTMP;
 
@@ -18,14 +24,44 @@ public class SudokuCell : Selectable
         _numberTMP = GetComponentInChildren<TextMeshProUGUI>();
     }
 
-    public void SetID(int row, int column)
+    /// <summary>
+    /// This will store the cell's initial data.
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <param name="answer"></param>
+    public void SetCellData(int row, int column, int answer)
     {
         ID = new Vector2(row, column);
+        Answer = answer;
     }
 
-    public void SetNumber(int number)
+    /// <summary>
+    /// This will handle the player's input to the cell.
+    /// </summary>
+    /// <param name="number"></param>
+    public void EnterValue(int number)
     {
-        Number = number;
-        _numberTMP.text = number.ToString();
+        Value = number;
+        ChangeText(number);
+    }
+
+    /// <summary>
+    /// This will make the cell as a clue.
+    /// Also prevents the clickable state. 
+    /// </summary>
+    public void SetCellAsClue()
+    {
+        IsClue = true;
+        interactable = false;
+    }
+
+    /// <summary>
+    /// Changes the cell's the text.
+    /// </summary>
+    /// <param name="number"></param>
+    private void ChangeText(int number)
+    {
+        _numberTMP.text = number != 0 ? number.ToString() : "";
     }
 }
