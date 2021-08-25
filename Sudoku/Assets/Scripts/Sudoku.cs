0512cd820005664e8950365c8823bda54991edde
Caglar <asdwas94@hotmail.com>
2021-08-25 20:56:25 +0300

    Added New const

--- Sudoku/Assets/Scripts/Sudoku.cs
+++ Sudoku/Assets/Scripts/Sudoku.cs
@@ -3,4 +3,7 @@ public static class Sudoku
     public const int RowCount = 9;
     public const int ColumnCount = 9;
     public const int TotalCellCount = 81;
+    public const int EmptyCell = 0;
+    public const int RegionRowCount = 3;
+    public const int RegionColumnCount = 3;
 }
