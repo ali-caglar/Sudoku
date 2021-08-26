0a4cba64be3e00d78fa2481102ee356c2012ed15
Caglar <asdwas94@hotmail.com>
2021-08-26 23:37:52 +0300

    Updated Grid creation now inherits from creator

--- Sudoku/Assets/Scripts/SudokuBoard.cs
+++ Sudoku/Assets/Scripts/SudokuBoard.cs
@@ -1,151 +1,23 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-public class SudokuBoard : MonoBehaviour
+public class SudokuBoard : GridCreator
 {
     /// The sudoku data that contains the grid
     [Header("Sudoku Data")] 
     [SerializeField] private SudokuData _sudokuData;
-
-    /// The number of rows to display
-    [Header("Board Size")] 
-    [SerializeField] private int _numberOfRows = 9;
-
-    /// The number of columns to display
-    [SerializeField] private int _numberOfColumns = 9;
-
-    /// The internal margin between the top of the sudoku board and the first cell
-    [Header("Board Padding")] 
-    [SerializeField] private int _paddingTop = 20;
-
-    /// The internal margin between the right of the sudoku board and the last cell
-    [SerializeField] private int _paddingRight = 20;
-
-    /// The internal margin between the bottom of the sudoku board and the last cell
-    [SerializeField] private int _paddingBottom = 20;
-
-    /// The internal margin between the left of the sudoku board and the first cell
-    [SerializeField] private int _paddingLeft = 20;
-
-    /// The prefab of the sudoku cell
-    [Header("Sudoku Cell")] 
-    [SerializeField] private GameObject _cellPrefab;
-
-    /// The horizontal and vertical margin to apply between cell's rows and columns
-    [SerializeField] private Vector2 _cellMargin = new Vector2(5, 5);
-
-    /// The grid layout used to display the sudoku board in rows and columns
-    public GridLayoutGroup SudokuBoardGrid { get; private set; }
-
+    
     /// The cell that selected by the player
     public SudokuCell CurrentlySelectedSudokuCell { get; private set; }
 
-    /// The horizontal and vertical size of the cells
-    private Vector2 _cellSize = new Vector2(50, 50);
-
-    /// The rect transform object of this gameobject
-    private RectTransform _rectTransform;
-
-    /// The slots that created by the board
-    private GameObject[,] _slotsOfSudokuBoard;
-
-    private void Awake()
+    protected override void Start()
     {
-        _rectTransform = GetComponent<RectTransform>();
-    }
-
-    private void Start()
-    {
-        ResizeCell();
-        AddGridLayoutGroup();
-        ResizeBoard();
-        CreateSudokuBoard();
+        base.Start();
         CreateSudokuGame();
     }
 
     public void SelectSudokuCell(SudokuCell sudokuCell)
     {
         CurrentlySelectedSudokuCell = sudokuCell;
-    }
-
-    /// <summary>
-    /// Resizes the sudoku cells, depends the screen size, the padding and margin.
-    /// </summary>
-    private void ResizeCell()
-    {
-        Canvas rootCanvas = GetComponentInParent<Canvas>().rootCanvas;
-
-        float widthOfScreen = Screen.width / rootCanvas.scaleFactor;
-        float widthOfMargin = _paddingLeft + _cellMargin.x * (_numberOfColumns - 1) + _paddingRight;
-
-        float newSize = (widthOfScreen - widthOfMargin) / _numberOfColumns;
-
-        _cellSize = new Vector2(newSize, newSize);
-    }
-
-    /// <summary>
-    /// Adds a grid layout group if there isn't one.
-    /// </summary>
-    private void AddGridLayoutGroup()
-    {
-        if (GetComponentInChildren<GridLayoutGroup>() == null)
-        {
-            GameObject boardGrid = new GameObject("SudokuBoardGrid");
-            boardGrid.transform.parent = transform;
-            boardGrid.transform.position = transform.position;
-            boardGrid.transform.localScale = Vector3.one;
-
-            SudokuBoardGrid = boardGrid.AddComponent<GridLayoutGroup>();
-        }
-
-        if (SudokuBoardGrid == null)
-        {
-            SudokuBoardGrid = GetComponentInChildren<GridLayoutGroup>();
-        }
-
-        SudokuBoardGrid.padding.top = _paddingTop;
-        SudokuBoardGrid.padding.right = _paddingRight;
-        SudokuBoardGrid.padding.bottom = _paddingBottom;
-        SudokuBoardGrid.padding.left = _paddingLeft;
-        SudokuBoardGrid.cellSize = _cellSize;
-        SudokuBoardGrid.spacing = _cellMargin;
-    }
-
-    /// <summary>
-    /// Resizes the sudoku board's panel, depends the number of rows/columns, the padding and margin.
-    /// </summary>
-    private void ResizeBoard()
-    {
-        float newWidth = _paddingLeft + _cellSize.x * _numberOfColumns + _cellMargin.x * (_numberOfColumns - 1) +
-                         _paddingRight;
-        float newHeight = _paddingTop + _cellSize.y * _numberOfRows + _cellMargin.y * (_numberOfRows - 1) +
-                          _paddingBottom;
-
-        Vector2 newSize = new Vector2(newWidth, newHeight);
-        _rectTransform.sizeDelta = newSize;
-        SudokuBoardGrid.GetComponent<RectTransform>().sizeDelta = newSize;
-    }
-
-    /// <summary>
-    /// Creates the sudoku slots.
-    /// </summary>
-    private void CreateSudokuBoard()
-    {
-        _slotsOfSudokuBoard = new GameObject[_numberOfRows, _numberOfColumns];
-        
-        for (int row = 0; row < _numberOfRows; row++)
-        {
-            for (int column = 0; column < _numberOfColumns; column++)
-            {
-                GameObject slot = Instantiate(_cellPrefab, SudokuBoardGrid.transform);
-
-                slot.transform.position = transform.position;
-                slot.GetComponent<RectTransform>().localScale = Vector3.one;
-                slot.name = $"Slot {row},{column}";
-                
-                _slotsOfSudokuBoard[row, column] = slot;
-            }
-        }
     }
 
     /// <summary>
@@ -160,7 +32,7 @@ public class SudokuBoard : MonoBehaviour
         {
             for (int column = 0; column < _numberOfColumns; column++)
             {
-                GameObject slot = _slotsOfSudokuBoard[row, column];
+                GameObject slot = _slotsOfGrid[row, column];
                 SudokuCell sudokuCell = slot.AddComponent<SudokuCell>();
 
                 // Sending initial data to cell
--- Sudoku/Assets/Scripts/SudokuInputManager.cs
+++ Sudoku/Assets/Scripts/SudokuInputManager.cs
@@ -1,122 +1,31 @@
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
-public class SudokuInputManager : MonoBehaviour
+public class SudokuInputManager : GridCreator
 {
-    [SerializeField] private GameObject _numberButton;
-
-    /// The internal margin between the top of the buttons grid and the first button
-    [Header("Grid Padding")] 
-    [SerializeField] private int _paddingTop = 20;
-
-    /// The internal margin between the right of the buttons grid and the last button
-    [SerializeField] private int _paddingRight = 20;
-
-    /// The internal margin between the bottom of the buttons grid and the last button
-    [SerializeField] private int _paddingBottom = 20;
-
-    /// The internal margin between the left of the buttons grid and the first button
-    [SerializeField] private int _paddingLeft = 20;
-
-    /// The margin to apply between buttons
-    [SerializeField] private float _margin = 10;
-
-    /// The horizontal and vertical size of the buttons
-    private Vector2 _buttonSize = new Vector2(50, 50);
-
-    /// The grid layout used to display the sudoku input numbers
-    public HorizontalLayoutGroup SudokuInputGrid { get; private set; }
-    
-    /// The rect transform object of this gameobject
-    private RectTransform _rectTransform;
-
-    private void Awake()
+    protected override void Start()
     {
-        _rectTransform = GetComponent<RectTransform>();
-    }
-
-    private void Start()
-    {
-        ResizeButton();
-        AddGridLayoutGroup();
-        ResizeBoard();
+        base.Start();
         CreateNumberButtons();
     }
-
-    /// <summary>
-    /// Resizes the input numbers, depends the screen size, the padding and margin.
-    /// </summary>
-    private void ResizeButton()
-    {
-        Canvas rootCanvas = GetComponentInParent<Canvas>().rootCanvas;
-
-        float widthOfScreen = Screen.width / rootCanvas.scaleFactor;
-        float widthOfMargin = _paddingLeft + _margin * (9 - 1) + _paddingRight;
-
-        float newSize = (widthOfScreen - widthOfMargin) / 9;
-
-        _buttonSize = new Vector2(newSize, newSize);
-    }
-
-    /// <summary>
-    /// Adds a horizontal layout group if there isn't one.
-    /// </summary>
-    private void AddGridLayoutGroup()
-    {
-        if (GetComponentInChildren<HorizontalLayoutGroup>() == null)
-        {
-            GameObject inputGrid = new GameObject("SudokuInputGrid");
-            inputGrid.transform.parent = transform;
-            inputGrid.transform.position = transform.position;
-            inputGrid.transform.localScale = Vector3.one;
-
-            SudokuInputGrid = inputGrid.AddComponent<HorizontalLayoutGroup>();
-        }
-
-        if (SudokuInputGrid == null)
-        {
-            SudokuInputGrid = GetComponentInChildren<HorizontalLayoutGroup>();
-        }
-
-        SudokuInputGrid.padding.top = _paddingTop;
-        SudokuInputGrid.padding.right = _paddingRight;
-        SudokuInputGrid.padding.bottom = _paddingBottom;
-        SudokuInputGrid.padding.left = _paddingLeft;
-        SudokuInputGrid.spacing = _margin;
-    }
-
-    /// <summary>
-    /// Resizes the input buttons' panel, depends the padding and margin.
-    /// </summary>
-    private void ResizeBoard()
-    {
-        float newWidth = _paddingLeft + _buttonSize.x * 9 + _margin * (9 - 1) + _paddingRight;
-        float newHeight = _paddingTop + _buttonSize.y + _margin + _paddingBottom;
-
-        Vector2 newSize = new Vector2(newWidth, newHeight);
-        _rectTransform.sizeDelta = newSize;
-        SudokuInputGrid.GetComponent<RectTransform>().sizeDelta = newSize;
-    }
-
+    
     /// <summary>
     /// Creates the input numbers.
     /// </summary>
     private void CreateNumberButtons()
     {
-        for (int sudokuNumber = 1; sudokuNumber <= 9; sudokuNumber++)
+        for (int row = 0; row < _numberOfRows; row++)
         {
-            GameObject numberSlot = Instantiate(_numberButton, SudokuInputGrid.transform);
+            for (int column = 0; column < _numberOfColumns; column++)
+            {
+                GameObject slot = _slotsOfGrid[row, column];
+                
+                TextMeshProUGUI numberTMP = slot.GetComponentInChildren<TextMeshProUGUI>();
+                numberTMP.text = $"{column + 1}"; // This will return button number
 
-            numberSlot.transform.position = transform.position;
-            numberSlot.GetComponent<RectTransform>().localScale = Vector3.one;
-            numberSlot.name = $"Number {sudokuNumber}";
-
-            TextMeshProUGUI numberTMP = numberSlot.GetComponentInChildren<TextMeshProUGUI>();
-            numberTMP.text = sudokuNumber.ToString();
-
-            SudokuInputButton inputButton = numberSlot.AddComponent<SudokuInputButton>();
-            inputButton.SetNumber(sudokuNumber);
+                SudokuInputButton inputButton = slot.AddComponent<SudokuInputButton>();
+                inputButton.SetNumber(column + 1);
+            }
         }
     }
 }
